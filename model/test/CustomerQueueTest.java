2ca845bcd16e6ae1b1485b2a914fb2c6105d3776
Michael <md42884p@pace.edu>
2025-12-11 13:20:01 -0500

    started making my test - mike

--- model/test/CustomerQueueTest.java
+++ model/test/CustomerQueueTest.java
@@ -1,5 +1,18 @@
 package model.test;
 
+
+import model.CustomerQueue;
+import model.Customer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class CustomerQueueTest {
     
+    private CustomerQueue queue;
+
+     @BeforeEach
+    void setUp() {
+        queue = new CustomerQueue();
+
+    }
 }
