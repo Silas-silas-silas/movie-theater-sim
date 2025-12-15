fde6a192b1934dcdb2ccfa3320b3c5f8abfe23f5
SilasGonzalez <sg29338p@pace.edu>
2025-12-15 16:24:14 -0500

    tested

---
+++ model/test/TransactionsTest.java
@@ -0,0 +1,57 @@
+package model;
+
+import java.util.Stack;
+
+public class TransactionsTest {
+
+    private static class Transaction {
+        private String movieName;
+        private double price;
+
+        public Transaction(String movieName, double price) {
+            this.movieName = movieName;
+            this.price = price;
+        }
+
+        public String getMovieName() {
+            return movieName;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+
+    private Stack<Transaction> history;
+
+    public TransactionsTest() {
+        history = new Stack<>();
+    }
+
+    public void logSale(String movieName, double price) {
+        history.push(new Transaction(movieName, price));
+    }
+
+    public double getTotalRevenue() {
+        double total = 0;
+        for (Transaction t : history) {
+            total += t.getPrice();
+        }
+        return total;
+    }
+
+    public double getRevenueForMovie(String movieName) {
+        double total = 0;
+        for (Transaction t : history) {
+            if (t.getMovieName().equals(movieName)) {
+                total += t.getPrice();
+            }
+        }
+        return total;
+    }
+
+    public int getTransactionCount() {
+        return history.size();
+    }
+}
+
